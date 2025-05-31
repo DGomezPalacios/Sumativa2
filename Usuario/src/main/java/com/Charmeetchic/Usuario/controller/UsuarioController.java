aac3480d2a5063c6c33d7ab921931fff8a5f36d9
DGomezPalacios <da.gomezp@duocuc.cl>
2025-05-30 21:31:40 -0400

    usuario completo

---
+++ Usuario/src/main/java/com/Charmeetchic/Usuario/controller/UsuarioController.java
@@ -0,0 +1,60 @@
+package com.Charmeetchic.Usuario.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Charmeetchic.Usuario.model.Usuario;
+import com.Charmeetchic.Usuario.service.UsuarioService;
+
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+        @GetMapping
+    public List<Usuario> getAllUsuarios() {
+        return usuarioService.getAllUsuarios();
+    }
+
+    // GET: Busca usuario por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.getUsuarioById(id);
+        return usuario.map(ResponseEntity::ok)
+                      .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<Usuario> registrar(@RequestBody Usuario usuario){
+        return ResponseEntity.ok(usuarioService.registrar(usuario)); //llama al servicio)
+    }
+
+    @PostMapping ("/login")
+    public ResponseEntity<?> login(@RequestBody Usuario usuario) {
+        Optional<Usuario> autenticado = usuarioService.login(usuario.getCorreo(), usuario.getContrasenia());
+        
+        //si se encuentra y la contraseña coincide devuev 200
+        return autenticado.isPresent()
+        ? ResponseEntity.ok(autenticado.get())
+        : ResponseEntity.status(401).body("Credenciales incomrrectas");
+    }
+
+    @PutMapping ("/actualizar")
+    public ResponseEntity<Usuario> actualizar(@RequestBody Usuario usuario){
+        return ResponseEntity.ok(usuarioService.actualizarPerfil(usuario));
+    }
+
+}
---
+++ Usuario/src/main/java/com/Charmeetchic/Usuario/service/UsuarioService.java
@@ -0,0 +1,44 @@
+package com.Charmeetchic.Usuario.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Charmeetchic.Usuario.model.Usuario;
+import com.Charmeetchic.Usuario.repository.UsuarioRepository;
+
+
+@Service
+
+public class UsuarioService {
+    @Autowired
+
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario registrar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> login(String correo, String contrasena) {
+        Optional<Usuario> user = usuarioRepository.findByCorreo(correo);
+        if (user.isPresent() && user.get().getContrasenia().equals(contrasena)) {
+            return user; // login exitoso
+        }
+        return Optional.empty(); //credenciales incorectas
+    }
+
+    public Usuario actualizarPerfil(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+    public List<Usuario> getAllUsuarios() {
+    return usuarioRepository.findAll();
+    }
+    public Optional<Usuario> getUsuarioById(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    
+
+}
