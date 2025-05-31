26f31b17c4d771ac4281184e4b2b2cf862a57a31
DGomezPalacios <da.gomezp@duocuc.cl>
2025-05-30 21:06:29 -0400

    estructura microservicios

---
+++ CargaMasiva/src/main/java/com/Charmeetchic/CargaMasiva/CargaMasivaApplication.java
@@ -0,0 +1,13 @@
+package com.Charmeetchic.CargaMasiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaMasivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaMasivaApplication.class, args);
+	}
+
+}
---
+++ CargaMasiva/src/test/java/com/Charmeetchic/CargaMasiva/CargaMasivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Charmeetchic.CargaMasiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargaMasivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
