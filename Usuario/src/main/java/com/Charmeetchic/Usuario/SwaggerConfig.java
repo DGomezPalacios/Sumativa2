9db102529f45fc983f3c2525d94dc69ac8e17f19
DGomezPalacios <da.gomezp@duocuc.cl>
2025-06-30 18:54:36 -0400

    funcional

--- Usuario/src/main/java/com/Charmeetchic/Usuario/SwaggerConfig.java
+++ Usuario/src/main/java/com/Charmeetchic/Usuario/SwaggerConfig.java
@@ -1,5 +1,19 @@
 package com.Charmeetchic.Usuario;
 
-public class SwaggerConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("API de Usuarios")
+                .version("1.0")
+                .description("Documentación de endpoints del microservicio Usuario"));
+    }
 }
