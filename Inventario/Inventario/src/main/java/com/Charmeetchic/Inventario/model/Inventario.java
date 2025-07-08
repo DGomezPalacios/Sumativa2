cc6f413b4d09a082ec2ed21a8b4e5b2e9bd4424c
BertaSoto <bert.soto@duocuc.cl>
2025-07-07 23:39:09 -0400

    Arreglo

---
+++ Inventario/Inventario/src/main/java/com/Charmeetchic/Inventario/model/Inventario.java
@@ -0,0 +1,24 @@
+package com.Charmeetchic.Inventario.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data 
+public class Inventario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+    private Long productoId;
+    private int stock; 
+    private int stockMinimo; // stock minimo para generar alerta
+    private boolean alerta; // alerta en caso de bajar stock
+    
+    @Version
+    private Integer version;
+    
+    public void verificarAlerta() {
+        this.alerta = stock <= stockMinimo;
+    }
+}
