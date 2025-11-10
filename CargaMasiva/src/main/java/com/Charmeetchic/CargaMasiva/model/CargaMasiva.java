b7978a5d9523e4eb0e433d8a6c87f58874112618
DGomezPalacios <da.gomezp@duocuc.cl>
2025-11-10 12:20:48 -0300

    arreglo model carga

--- CargaMasiva/src/main/java/com/Charmeetchic/CargaMasiva/model/CargaMasiva.java
+++ CargaMasiva/src/main/java/com/Charmeetchic/CargaMasiva/model/CargaMasiva.java
@@ -14,22 +14,7 @@ public class CargaMasiva {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String archivoCSV;
     private Date fechaCarga;
+    private Integer cantidadProductos;
 
-    // Métodos de importación
-    public void importarProductos() {
-        System.out.println("Importando productos desde archivo");
-        // Aquí iría la lógica de lectura del CSV y carga a la base de datos
-    }
-
-    public void importarCategorias() {
-        System.out.println("Importando categorías desde archivo");
-        // Aquí iría la lógica correspondiente
-    }
-
-    public void importarStock() {
-        System.out.println("Importando stock desde archivo");   
-        // Aquí iría la lógica correspondiente
-    }
 }
