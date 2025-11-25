c7346e365d680774a393bc64b57aeb391282c2fa
berta <bert.soto@duocuc.cl>
2025-11-25 11:45:21 -0300

    Contacto

---
+++ Contacto/src/test/java/com/Charmeetchic/Contacto/ContactoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Charmeetchic.Contacto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
